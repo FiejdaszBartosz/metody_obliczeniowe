ec1bfa2986435b2d5e638dc23375436d4a04163e
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-04-07 14:20:56 +0200

    Finish all functions to zad5

--- zad5/main.cpp
+++ zad5/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 /**
  * Tworzy macierz kwadratowa o podanym rozmiarze
@@ -52,11 +53,95 @@ void uzupelnijMacierz(double **pMacierz, int n) {
  * @param n rozmiar macierzy
  * @param index indeks w macierzy
  */
-void wyswietlMacierz(double **pMacierz, int n, int* index) {
+void wyswietlMacierz(double **pMacierz, int n, int *index) {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j)
       std::cout << "  " << pMacierz[index[i]][j] << "\t";
     std::cout << "\n";
+  }
+}
+
+/**
+ * Realizuje czesciowy wybor elementu podstawowego. Potrzebna aby usiknac dzielenia przez zero
+ * @param pMacierz wskaznik do macierzy
+ * @param n rozmiar macierzy
+ * @param j pozycja w index
+ * @param index
+ * @return wiersz po zamianie
+ */
+int elementPodstawowy(double **pMacierz, int n, int j, int *index) {
+  int wiersz;
+  for (int i = j; i < n; i++) {
+    if (std::fabs(pMacierz[index[i]][j]) < std::fabs(pMacierz[index[i + 1]][j]))
+      wiersz = index[i + 1];
+    else
+      wiersz = index[i];
+  }
+
+  return wiersz;
+}
+
+void gauss(double **pMacierz, int *index) {
+  int wiersz;
+  double v;
+  for (int k = 0; k < 3; k++) {
+    if (pMacierz[index[k]][index[k]] == 0.0) {
+      wiersz = elementPodstawowy(pMacierz, index[k], 3, index);
+      // Zapisanie zmian w tablicy indeksow
+      index[wiersz] = index[k];
+      index[k] = wiersz;
+    }
+    for (int i = k + 1; i < 4; i++) {
+      v = pMacierz[index[i]][k];
+      for (int j = k + 1; j < 4; j++) {
+        // Obliczanie wartosci dla macierzy U
+        pMacierz[index[i]][j] = pMacierz[index[i]][j] - pMacierz[index[k]][j] * (v / pMacierz[index[k]][k]);
+        // Zapisanie macierzy L
+        // wspolczynniki przez ktore mnozymy kolejne wiersze (A[i][k] / A[k][k])
+        pMacierz[index[i]][k] = v / pMacierz[index[k]][k];
+      }
+    }
+  }
+}
+
+/*
+ * Aby roziwazac rownanie:
+ * A * x = b
+ * Korzystamy z tego ze:
+ * A = L * U
+ * Oraz tworzymy uklad rowanan:
+ * L * y = b
+ * y = U * x
+ */
+
+/**
+ * Wyznaczenie y potrzebnego do rozwiazania powyzszego ukladu rownan
+ * @param macierzL maczierz L (trojkatna dolna)
+ * @param wektorB wektor b
+ * @param index tablica indexow
+ * @param n rozmiar macierzy
+ */
+void wyznaczY(double **macierzL, double *wektorB, int *index, int n) {
+  double suma = 0.0;
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j < i; j++)
+      suma += macierzL[index[i]][j] * wektorB[index[j]];
+
+    wektorB[index[i]] = (wektorB[index[i]] - suma) / 1.0;     //Na glownej przekatnej sa 1
+
+    suma = 0.0;
+  }
+}
+
+void wyznaczU(double **macierzU, double *wektorB, int *index, int n) {
+  double suma = 0.0;
+  for (int i = n; i >= 0; i--) {
+    for (int j = i + 1; j <= n; j++)
+      suma += macierzU[index[i]][j] * wektorB[index[j]];
+
+    wektorB[index[i]] = (wektorB[index[i]] - suma) / (macierzU[index[i]][i]);
+
+    suma = 0.0;
   }
 }
 
