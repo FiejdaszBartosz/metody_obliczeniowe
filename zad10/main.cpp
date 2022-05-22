3b478ca36dfa7bfa3a2f08a379a3ade8bcb83fab
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2022-05-22 13:56:10 +0200

    ?

--- zad10/main.cpp
+++ zad10/main.cpp
@@ -2,3 +2,145 @@
 // Created by Bartosz Fiejdasz on 21/05/2022.
 //
 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+double funkcjaBezposrednia(double dt, double tk, double yk) {
+  return yk + dt * (-((10.0 * tk * tk + 20.0) / (tk * tk + 1.0)) * (yk - 1.0));
+}
+double funkcjaAnalityczna(double tk) { return 1 - exp(-10.0 * (tk + atan(tk))); }
+double funkcjaPosrednia(double dt, double tk, double y_prev) {
+  double y, temp;
+  temp = (10.0 * (tk + dt) * (tk + dt) + 20.0) / ((tk + dt) * (tk + dt) + 1.0);
+  y = (y_prev + dt * temp) / (1 + dt * temp);
+  return y;
+}
+double funkcjaPosredniaBlad(double dt) {
+  double y, diff, tk = 0, maxDiff = 0.0, y_poprzednie = 0.0;
+  while (tk < 1.0) {
+    y = funkcjaPosrednia(dt, tk, y_poprzednie);
+    diff = fabs(funkcjaAnalityczna(tk) - y_poprzednie);
+    y_poprzednie = y;
+    if (diff > maxDiff) maxDiff = diff;
+    tk += dt;
+  }
+  return maxDiff;
+}
+
+double funkcjaBezposredniaBlad(double dt) {
+  double y, maxDiff = 0.0, diff, tk = 0, y_poprzednie = 0.0;
+  while (tk < 1.0) {
+    y = funkcjaBezposrednia(dt, tk, y_poprzednie);
+    diff = fabs(funkcjaAnalityczna(tk) - y_poprzednie);
+    y_poprzednie = y;
+    if (diff > maxDiff)
+      maxDiff = diff;
+    tk += dt;
+  }
+  return maxDiff;
+}
+double funkcjaTrapezow(double dt, double tk, double y_poprzedni) {
+  double y, tempf, tempf1, i;
+  while (i < tk) {
+    tempf = ((10.0 * i * i + 20.0) / (i * i + 1.0)); //część f(tk,yk)
+    tempf1 = (10.0 * (i + dt) * (i + dt) + 20.0) / ((i + dt) * (i + dt) + 1.0);
+    y = ((-dt / 2.0) * (tempf * (y_poprzedni - 1.0) - tempf1) + y_poprzedni) / (1.0
+        + (dt / 2.0) * tempf1);
+    i += dt;
+  }
+  return y;
+}
+double funkcjaTrapezowBlad(double dt) {
+  double diff, tk = 0, y, maxDiff = 0.0, y_poprzedni = 0.0;
+  while (tk < 1.0) {
+    y = funkcjaTrapezow(dt, tk, y_poprzedni);
+    diff = fabs(funkcjaAnalityczna(tk) - y_poprzedni);
+    y_poprzedni = y;
+    if (diff > maxDiff)
+      maxDiff = diff;
+    tk += dt;
+  }
+  return maxDiff;
+}
+
+int main() {
+  std::fstream diffBezposrednia, diffPosrednia, diffTrapezow, resultsBezposredniaSTB, resultsPosrednia, resultsTrapezow,
+      resultsAnalitycznie, resultsBezposredniaNST; //tworzenie plików
+  diffBezposrednia.open("diffBezposrednia.txt", std::fstream::out); // otwieramy do zapisu
+  diffPosrednia.open("diffPosrednia.txt", std::fstream::out);
+  diffTrapezow.open("diffTrapezow.txt", std::fstream::out);
+  resultsBezposredniaSTB.open("resultsBezposredniaSTB.txt", std::fstream::out);
+  resultsPosrednia.open("resultsPosrednia.txt", std::fstream::out);
+  resultsTrapezow.open("resultsTrapezow.txt", std::fstream::out);
+  resultsAnalitycznie.open("resultsAnalitycznie.txt", std::fstream::out);
+  resultsBezposredniaNST.open("resultsBezposredniaNST.txt", std::fstream::out);
+  resultsBezposredniaSTB << std::scientific; //ustawienie precyzji
+  resultsPosrednia << std::scientific;
+  resultsTrapezow << std::scientific;
+  resultsAnalitycznie << std::scientific;
+  resultsBezposredniaNST << std::scientific;
+  diffBezposrednia << std::scientific;
+  diffPosrednia << std::scientific;
+  diffTrapezow << std::scientific;
+  double dt = 0.005, y_nastepne, yn = 0, tk = 0, blad, y_poprzednie;
+//analityczna
+  while (tk < 5.0) {
+    y_nastepne = funkcjaAnalityczna(tk);
+    resultsAnalitycznie << tk << " " << y_nastepne << "\n";
+    tk += dt;
+  }
+  tk = 0;
+//bezpośrednia - stabilna
+  while (tk < 1.0) {
+    y_nastepne = funkcjaBezposrednia(dt, tk, yn);
+    yn = y_nastepne;
+    resultsBezposredniaSTB << tk << " " << y_nastepne << "\n";
+    tk += dt;
+  }
+  dt = 0.2, yn = 0, tk = 0;
+//bezpośrednia - niestabilna
+  while (tk < 5.0) {
+    y_nastepne = funkcjaBezposrednia(dt, tk, yn);
+    resultsBezposredniaNST << tk << " " << y_nastepne << "\n";
+    yn = y_nastepne;
+    tk += dt;
+  }
+  dt = 0.005, tk = 0;
+  y_poprzednie = 0.0;
+//pośrednia
+  while (tk < 1.0) {
+    y_nastepne = funkcjaPosrednia(dt, tk, y_poprzednie);
+    y_poprzednie = y_nastepne;
+    resultsPosrednia << tk << " " << y_nastepne << "\n";
+    tk += dt;
+  }
+  tk = 0;
+//trapezów
+  y_poprzednie = 0.0;
+  while (tk < 1.0) {
+    y_nastepne = funkcjaTrapezow(dt, tk, y_poprzednie);
+    y_poprzednie = y_nastepne;
+    resultsTrapezow << tk << " " << y_nastepne << "\n";
+    tk += dt;
+  }
+// liczymy błędy maksymalne
+  dt = 0.1;
+  while (dt > 1e-14) {
+    blad = log10(funkcjaBezposredniaBlad(dt));
+    diffBezposrednia << log10(dt) << " " << blad << "\n";
+    blad = log10(funkcjaPosredniaBlad(dt));
+    diffPosrednia << log10(dt) << " " << blad << "\n";
+    blad = log10(funkcjaTrapezowBlad(dt));
+    diffTrapezow << log10(dt) << " " << blad << "\n";
+    dt = dt / 1.12;
+  }
+  resultsBezposredniaSTB.close(); //zamykamy pliki
+  resultsPosrednia.close();
+  resultsAnalitycznie.close();
+  resultsBezposredniaNST.close();
+  resultsTrapezow.close();
+  diffBezposrednia.close();
+  diffPosrednia.close();
+  diffTrapezow.close();
+}
--- zad9/main.cpp
+++ zad9/main.cpp
@@ -6,18 +6,38 @@
 #include <cmath>
 #include <fstream>
 
-const double p = 1.0, q = 0.0, r = -4.0; //współczynniki w równaniu
-const double alfa = 0.0, beta = 1.0, gamma = -1.0; //współczynniki z warunków brzegowych
-const double fi = 0.0, psi = 1.0, teta = 0.0; //psi dowolne
-const double poczatekPrzedzialu = 0.0, koniecPrzedzialu = 1.0; //granice przedziału
+// Odczytanie wspolczynnikow z zadanego rownania rozniczkowego
 
-double rownanieAnalityczne(double x) {    // wyliczone analitycznie
-  return (exp(2.0 - 2.0 * x) - 4 * exp(4.0 - x * 2.0) + 4.0 * exp(x * 2.0) - exp(2.0 + 2.0 * x) - x + x * exp(4.0))
+// Z zadanego równania różniczkowego
+const double P = 1.0, Q = 0.0, R = -4.0;
+
+// Z warunków brzegowych
+const double ALFA = 0.0, BETA = 1.0, GAMMA = -1.0;
+const double FI = 0.0, PSI = 1.0, TETA = 0.0;
+
+// Zadany przedzial
+const double POCZATEK_PRZEDZIALU = 0.0, KONIEC_PRZEDZIALU = 1.0;
+
+/**
+ * Rozwiazanie analityczne
+ * @param x zmienna x
+ * @return rozwizanie dla zmiennej x
+ */
+double rownanieAnalityczne(double x) {
+  return (exp(2.0 - 2.0 * x) - 4.0 * exp(4.0 - x * 2.0) + 4.0 * exp(x * 2.0) - exp(2.0 + 2.0 * x) - x + x * exp(4.0))
       / (4.0 - 4 * exp(4.0));
 }
 
+/**
+ * Realizacja algorytmu Thomasa, zmienne podpisane jak na wykladzie
+ * @param l wektor l
+ * @param d wektor d
+ * @param u wektor u
+ * @param b wektor b
+ * @param x wektor wynikowy x
+ * @param N rozmiar wektorow
+ */
 void thomas(double *l, double *d, double *u, double *b, double *x, int N) {
-  // Urzywam kopi aby uniknac zmienniania oryginalnych danych
   double *bKopia = new double[N];
   double *dKopia = new double[N];
 
@@ -25,17 +45,21 @@ void thomas(double *l, double *d, double *u, double *b, double *x, int N) {
   dKopia[0] = d[0];
   bKopia[0] = b[0];
 
+  // wyliczenie ni
   for (int i = 1; i < N; i++) {
     dKopia[i] = d[i] - l[i - 1] * (u[i - 1] / dKopia[i - 1]);
   }
 
+  // wyliczenie r
   for (int i = 1; i < N; i++) {
     bKopia[i] = b[i] - l[i - 1] * bKopia[i - 1] / dKopia[i - 1];
   }
 
-  x[N - 1] = bKopia[N - 1] / dKopia[N - 1];   // Obliczenie x n-1
+  // Obliczanie rozwiazania
+  x[N - 1] = bKopia[N - 1] / dKopia[N - 1];
 
-  for (int i = N - 2; i >= 0; i--) {    // Obliczenie pozostalych x
+  // Obliczenie pozostalych x
+  for (int i = N - 2; i >= 0; i--) {
     x[i] = (bKopia[i] - u[i] * x[i + 1]) / dKopia[i];
   }
 
@@ -43,6 +67,12 @@ void thomas(double *l, double *d, double *u, double *b, double *x, int N) {
   delete[] dKopia;
 }
 
+/**
+ * Znajduje najwiekszy blad i zwraca jego index (Norma maximum)
+ * @param blad wektor bledu
+ * @param N rozmiar
+ * @return index
+ */
 int znajdzNajwiekszyBlad(double *blad, int N) {
   double maksymalny = fabs(blad[0]);    // Inicjalizujemy blad pierwsza wartoscia wektora
   int index = 0;
@@ -55,6 +85,7 @@ int znajdzNajwiekszyBlad(double *blad, int N) {
 
   return index;
 }
+
 /**
  * Funkcja realizująca dyskretyzację Numerowa
  * @param h krok
@@ -62,14 +93,14 @@ int znajdzNajwiekszyBlad(double *blad, int N) {
  * @return blad
  */
 double Numerowa(double h, int N) {
-  double *l, *d, *u, *b, *x, *blad, x0 = poczatekPrzedzialu, xn = poczatekPrzedzialu;
+  double *l, *d, *u, *b, *x, *blad, x0 = POCZATEK_PRZEDZIALU, xn = KONIEC_PRZEDZIALU;
   std::fstream numerow, analitycznie;
   numerow.open("wynikiNumerow.txt", std::ios_base::app);
   analitycznie.open("wynikiAnalitycznie.txt", std::ios_base::app);
   analitycznie << std::scientific;
   numerow << std::scientific;
   std::cout.precision(10);
-  l = new double[N]; //tworzymy wektory
+  l = new double[N];
   d = new double[N];
   u = new double[N];
   b = new double[N];
@@ -78,32 +109,37 @@ double Numerowa(double h, int N) {
 
   // Realizuje algorytm zgodnie ze wzorami z wykladu
 
-  u[0] = alfa / h;
-  d[0] = beta - alfa / h;
-  b[0] = -gamma;
+  // Z warunku brzegowego
+  u[0] = ALFA / h;
+  d[0] = BETA - ALFA / h;
+  b[0] = -GAMMA;
 
+  // Wyznaczenie srodkowych wyrazow w zapisie wektorowym
   for (int i = 1; i < N - 1; i++) {
-    l[i - 1] = p / (h * h) + r / 12.0;
-    d[i] = (-2.0 * p) / (h * h) + r * (10.0 / 12.0);
-    u[i] = p / (h * h) + r / 12.0;
+    l[i - 1] = P / (h * h) + R / 12.0;
+    d[i] = (-2.0 * P) / (h * h) + R * (10.0 / 12.0);
+    u[i] = P / (h * h) + R / 12.0;
     b[i] = (x0 + i * h - h) / 12.0 + (10.0 / 12.0) * (x0 + i * h) + (x0 + i * h + h) / 12.0;
   }
 
-  l[N - 2] = -fi / h; //końcowe wyrazy mają także specjalne dane
-  d[N - 1] = -fi / h + psi;
-  b[N - 1] = -teta;
+  // Z warunku brzegowego
+  l[N - 2] = -FI / h;
+  d[N - 1] = -FI / h + PSI;
+  b[N - 1] = -TETA;
 
-  thomas(l, d, u, b, x, N); // Algorytm Thomasa
+
+  // Rozwiazanie macierzy trojdiagonalnej algorytmem thomasa
+  thomas(l, d, u, b, x, N);
 
   // Obliczenie bledu między algotytmem numerowa, a rozwiązaniem analitycznym
   for (int i = 0; i < N; i++) {
     blad[i] = fabs(x[i] - rownanieAnalityczne(xn));
     xn += h;
   }
+
   int naj = znajdzNajwiekszyBlad(blad, N); //znajdujemy największy błąd
 
-  if (N == 162) //losowa liczba
-  {
+  if (N == 1002) {
     for (int i = 0; i < N; i++) {
       numerow << x0 << "\t" << x[i] << "\t\n";
       analitycznie << x0 << "\t" << rownanieAnalityczne(x0) << "\n";
@@ -123,9 +159,15 @@ double Numerowa(double h, int N) {
   return blad[naj];
 }
 
+/**
+ * Realizacja trzypunktowej dyskretyzacji konwencjonalnej
+ * @param h krok
+ * @param N rozmiar
+ * @return blad
+ */
 double konwencjonalnaTrzypunktowa(double h, int N) //funkcja realizująca dyskretyzację konwencjonalną trzypunktową
 {
-  double *l, *d, *u, *b, *x, *blad, x0 = poczatekPrzedzialu, xn = poczatekPrzedzialu;
+  double *l, *d, *u, *b, *x, *blad, x0 = POCZATEK_PRZEDZIALU, xn = KONIEC_PRZEDZIALU;
   std::fstream konwencjonalnie;
   konwencjonalnie.open("wynikiKonwencjonalnie.txt", std::ios_base::app);
   konwencjonalnie << std::scientific;
@@ -137,20 +179,23 @@ double konwencjonalnaTrzypunktowa(double h, int N) //funkcja realizująca dyskre
   x = new double[N];
   blad = new double[N];
 
-  u[0] = alfa / h;
-  d[0] = beta - alfa / h;
-  b[0] = -gamma;
+  // Z warunku brzegowego
+  u[0] = ALFA / h;
+  d[0] = BETA - ALFA / h;
+  b[0] = -GAMMA;
 
+  // Wyznaczenie srodkowych wyrazow w zapisie wektorowym
   for (int i = 1; i < N - 1; i++) {
-    l[i - 1] = p / (h * h) - q / (2.0 * h);
-    d[i] = (-2.0 * p) / (h * h) + r;
-    u[i] = p / (h * h) + q / (2.0 * h);
+    l[i - 1] = P / (h * h) - Q / (2.0 * h);
+    d[i] = (-2.0 * P) / (h * h) + R;
+    u[i] = P / (h * h) + Q / (2.0 * h);
     b[i] = (x0 + i * h);
   }
 
-  l[N - 2] = -fi / h;
-  d[N - 1] = -fi / h + psi;
-  b[N - 1] = -teta;
+  // Z warunku brzegowego
+  l[N - 2] = -FI / h;
+  d[N - 1] = -FI / h + PSI;
+  b[N - 1] = -TETA;
 
   thomas(l, d, u, b, x, N);
 
@@ -160,8 +205,7 @@ double konwencjonalnaTrzypunktowa(double h, int N) //funkcja realizująca dyskre
   }
 
   int naj = znajdzNajwiekszyBlad(blad, N);
-  if (N == 162) //losowa liczba
-  {
+  if (N == 1002) {
     for (int i = 0; i < N; i++) {
       konwencjonalnie << x0 << "\t" << x[i] << "\n";
       x0 += h;
@@ -177,19 +221,20 @@ double konwencjonalnaTrzypunktowa(double h, int N) //funkcja realizująca dyskre
   return blad[naj];
 }
 
-int main() {
+int main() //funkcja główna
+{
   double h, bladNum, bladKonw;
-  int i;
+  int N; //ilość iteracji
 
   std::fstream bledy, rzedy;
   bledy.open("bledy.txt", std::fstream::out);
   bledy << std::scientific; //ustawienie precyzji
   std::cout.precision(10);
 
-  for (i = 2; i < 30000; i += 80) {
-    h = (koniecPrzedzialu - poczatekPrzedzialu) / (i - 1);
-    bladKonw = (konwencjonalnaTrzypunktowa(h, i));
-    bladNum = (Numerowa(h, i));
+  for (N = 2; N < 100000; N += 100) {
+    h = (KONIEC_PRZEDZIALU - POCZATEK_PRZEDZIALU) / (N - 1);
+    bladKonw = (konwencjonalnaTrzypunktowa(h, N));
+    bladNum = (Numerowa(h, N));
     bledy << log10(h) << "\t" << log10(bladKonw) << "\t" << log10(bladNum) << "\n";
   }
   bledy.close();
